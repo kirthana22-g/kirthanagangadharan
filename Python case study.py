7fcb5b481a899e535fac022c5aa23c659c90e1b2
kirthana22-g <kirthiganga22@gmail.com>
2025-02-13 13:48:45 +0530

    Add files via upload

---
+++ Python case study.py
@@ -0,0 +1,104 @@
+PYTHON
+
+CASE STUDY 1
+# Concatenation of 3 strings from variable
+var = "welcome to"
+var2 = "python programming"
+print(var + var2)
+print(var + " " +var2)
+welcome topython programming
+welcome to python programming
+
+# Print a value from variable
+# Build calculator with 2 variables
+# Python Operators - Addition and Subtraction and mul and division and modulus
+
+
+a= int(input("enter number a:"))
+b=int(input("enter number b:"))
+print ("ouput:" ,a+b)
+print ("ouput:" ,a-b)
+print ("ouput:" ,a*b)
+print ("ouput:" ,a/b)
+print ("ouput:" ,a%b)
+enter number a:10
+enter number b:5
+ouput: 15
+ouput: 5
+ouput: 50
+ouput: 2.0
+ouput: 0
+
+# “Python” - what is the index value of letter “t”?
+word = "python"
+print(word.index("t"))
+2
+
+# Welcome‘ - Extract the 4th value from the given string.
+word="welcome"
+print(word[3])
+
+
+# Calculate the length of this word “Python”.
+print(len(word))
+
+
+# “I’m doing great‘ -. Write a python code to convert this to upper case
+a= "I'm doing great"
+print(a.upper())
+c
+7
+I'M DOING GREAT
+
+#Fill in the blanks - “_____ is my country” and “____ is learning ____”
+a="______ is my country" and "_____ is learning ____"
+print(a)
+#Check “learn” is exist in the given string - “I’m learning python”
+a="I'm learning python"
+print("learn" in a)
+#Check “Python” is not present in the given string “i’m learning NodeJs”
+word="i'm learning NodeJs"
+print("Python" not in word)
+_____ is learning ____
+True
+True
+
+# Get the character at position 1 of a string
+a="welcome to python"
+print(a[1])
+
+
+# Substring. Get the characters from position 2 to position 5 (not included)
+a="welcome to python"
+print(a[1])
+print(a[2:5])
+
+
+# Remove whitespace from the beginning or at the end of a string
+word= " heloo alll "
+print(word.strip())
+e
+e
+lco
+heloo alll
+
+#Return the length of a string
+word="welcome to python"
+print(len(word))
+#Convert a string to upper case, lower case
+word="welcome to python"
+print(word.upper())
+print(word.lower())
+#Replace a string with another string (Welcome Raja - Replace Raja with Roja)
+word="welcome Raja"
+print(word.replace("Raja","Roja"))
+#Split a string into different (‘hello,world’)
+word="hello,world"
+print(word.split(","))
+
+
+17
+WELCOME TO PYTHON
+welcome to python
+welcome Roja
+['hello', 'world']
